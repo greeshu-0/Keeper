bcb827d9056431256d3b7654beb974bf57349acf
greeshu-0 <greeshmajagarapu8878@gmail.com>
2025-06-19 14:33:19 +0530

    Initial commit

---
+++ src/Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+function Note() {
+  return (
+    <div className="note">
+      <h1>My First Note</h1>
+      <p>
+        Just started creating this from scratch but I failed many times beacuse
+        of git but I'll keep trying...
+      </p>
+    </div>
+  );
+}
+export default Note;
